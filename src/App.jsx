fe7ce0e9511e1a0992af3b175f86921c35ee2f3f
AniketBhatti <aniket.imscit20@gmail.com>
2024-10-22 17:35:36 +0530

    feat: 20-11-24 Rope Effect Added and 4th Section Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/LandingPage/Landing";
-import Three from "./components/ThreeJSModelsLWM/Three";
+// import Three from "./components/ThreeJSModelsLWM/Three";
 import CyberPunk from "./components/Cyberpunk/CyberPunk";
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
         <Routes>
           <Route index element={<Landing />} />
           <Route path="/" element={<Landing />} />
-          <Route path="/three" element={<Three />} />
+          {/* <Route path="/three" element={<Three />} /> */}
           <Route path="/cp" element={<CyberPunk  />} />
         </Routes>
       </BrowserRouter>
--- src/pages/LandingPage/Landing.jsx
+++ src/pages/LandingPage/Landing.jsx
@@ -1,18 +1,33 @@
 import React, { useRef } from "react";
 import "./landing.scss";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import { useScroll } from "framer-motion";
 import { Link } from "react-router-dom";
 
 const Landing = () => {
 
-    // function cubeAnimation(){
-  
-    //     gsap.to(".x", { x: 100, duration: 1, ease: "power1.inOut" });
+  const pathRef = useRef(null);
 
-    // }
+  React.useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
 
-    // cubeAnimation()
+    gsap.to(pathRef.current, {
+      scrollTrigger: {
+        trigger: pathRef.current,
+        start: "top center",
+        end: "bottom center",
+        scrub: 1,
+      },
+      attr: {
+        d: "M0 4.5C1902.08 3.82732 5151.34 2.67819 7273 1.92785"
+      },
+      ease: "none"
+    });
+  }, []);
+
+
 
   return (
     <main>
@@ -50,6 +65,21 @@ const Landing = () => {
           </h2>
         </section>
       </div>
+
+      <section className="transform-ideas">
+        <h2>
+          We transform ideas <div className="bulbs" /> into memorable
+          experiences <div className="squares" /> through branding{" "}
+          <div className="shapes" /> web design, and digital strategy.
+        </h2>
+      </section>
+
+
+      <section className="Line" style={{height:'100vh'}}>
+        <svg viewBox="0 0 1615 500" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path ref={pathRef} id="animatedPath" d="M0.5 162.5C301.5 339 1385 -211.5 1614 97" stroke="#ED0180" stroke-width="2"/>
+        </svg>
+      </section>
 
     </main>
   );
